0ee57a27a772d4bf814759885ffb55ee50bfa199
KyryloKireiev <90455454+KyryloKireiev@users.noreply.github.com>
2021-12-28 20:28:22 +0200

    algorithm problems

---
+++ algorithm problems/Inheritance of exceptions.py
@@ -0,0 +1,82 @@
+# the task
+# https://stepik.org/lesson/24463/step/7?unit=6771
+
+dict_graph = {}
+
+n = int(input())
+
+for i in range(n):
+    a, *b = input().replace(":", " ").split()
+    if a not in dict_graph:
+        dict_graph[a] = b
+    else:
+        for key, value in dict_graph:
+            if key == value:
+                dict_graph[key] = value + b
+
+print(dict_graph)
+
+
+def find_path(graph, start, end, path=[]):
+    path = path + [start]
+
+    if start == end:
+        return path
+
+    if start not in graph:
+        return None
+
+    for node in graph[start]:
+        if node not in path:
+            new_path = find_path(graph, node, end, path)
+            if new_path:
+                return path
+    return None
+
+
+m = int(input())
+exceptions_lst = []
+
+new_lst = []
+
+for i in range(m):
+    search_ex = input()
+    for exception in exceptions_lst:
+        if find_path(dict_graph, search_ex, exception) != None:
+            new_lst.append(search_ex)
+    exceptions_lst.append(search_ex)
+
+super_list = []
+
+for el in new_lst:
+    if el not in super_list:
+        super_list.append(el)
+
+for el in super_list:
+    print(el)
+
+
+# exceptions for check
+"""
+4
+ArithmeticError
+ZeroDivisionError : ArithmeticError
+OSError
+FileNotFoundError : OSError
+4
+ZeroDivisionError
+OSError
+ArithmeticError
+FileNotFoundError
+
+"""
+'''
+4
+BaseException
+Exception : BaseException
+LookupError : Exception 
+KeyError : LookupError
+2
+BaseException
+KeyError
+'''
